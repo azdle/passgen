d489d265ee3eb93b98a179e1a8e1983b176719dc
Patrick Barrett <patrick@psbarrett.com>
2016-10-14 17:56:19 -0500

    at least pretend I care about efficiency

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,13 @@ extern crate clap;
 fn main() {
     use clap::{Arg, App};
     use rand::{Rng, OsRng};
-    use std::ascii::AsciiExt;
+
+    let uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".chars();
+    let lowercase = "abcdefghijklmnopqrstuvwxyz".chars();
+    let numeric = "1234567890".chars();
+
+    let alpha = uppercase.chain(lowercase);
+    let alphanumeric = alpha.chain(numeric);
 
     let matches = App::new("passgen")
                           .version("0.1.0")
@@ -14,23 +20,24 @@ fn main() {
                                .help("Sets the desired password length")
                                .required(true)
                                .index(1))
-                          .arg(Arg::with_name("CHARSET")
+                          .arg(Arg::with_name("SYMBOLS")
                                .help("Sets the special characters to be used")
                                .required(false)
                                .index(2))
                           .get_matches();
 
     let length_str = matches.value_of("LENGTH").unwrap();
-    let length: u16 = length_str.parse().expect("Length must be number");
-    let charset = matches.value_of("CHARSET").unwrap_or("");
+    let length: usize = length_str.parse().expect("Length must be number");
+    let symbols = matches.value_of("SYMBOLS").unwrap_or("");
 
     let mut r = OsRng::new().expect("failed to get os rng");
 
-    let result = r
-        .gen_iter::<char>()
-        .filter(|x| (x.is_ascii() && x.is_alphanumeric()) || (charset.chars().filter(|y| y == x).count() > 0))
-        .take(length as usize)
-        .collect::<String>();
+    let charset: Vec<char> = alphanumeric.chain(symbols.chars()).collect();
+    let mut result = String::with_capacity(length);
+
+    for _ in 0..length {
+        result.push(*r.choose(&charset).unwrap());
+    }
 
     println!("{}", result);
 }
